7a93035648cc5a7b18cbae74367b62e3796d098d
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-06-19 14:24:32 +0900

    test

--- BuildTestTests/BuildTestTests.swift
+++ BuildTestTests/BuildTestTests.swift
@@ -24,6 +24,7 @@ class BuildTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("TEST!!!!!!!!")
     }
     
     func testPerformanceExample() {
